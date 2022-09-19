c1f50fb5907c4e2647b8f0adb25ed686b712ddf6
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-09-19 09:24:06 -0300

    Método GET part 1

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Lista do conteúdo relevante a todo curso Spring Boot Essentials 2. Para mais in
 - **04. @Component, @Autowired, @SpringBootAplication**
 - **05. Hot Swap com Spring Boot Devtools**
 - **06. Gerando projeto com start.spring.io**
-- **05. Padrão REST e POST**
+- **07. Método GET pt 01**
 - **06. Padrão REST e POST 2**
 - **07. Padrão REST, PUT e DELETE 3**
 - **08. Adicionando Spring Data JPA com MySQL**
--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,6 +1,7 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.service.AnimeService;
 import academy.devdojo.springboot2.util.DateUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -12,16 +13,17 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("anime")
+@RequestMapping("animes")
 @Log4j2
 @AllArgsConstructor
 public class AnimeController {
-    private DateUtil dateUtil;
+    private final DateUtil dateUtil;
+    private final AnimeService animeService;
 
-    @GetMapping(path = "list")
+    @GetMapping
     public List<Anime> list() {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("Boku No Hero"), new Anime("Bersek"));
+        return animeService.listAll();
     }
 
 }
