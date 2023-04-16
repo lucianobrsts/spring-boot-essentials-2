62f51d0af9e2ce383773b617bac74d19128312dd
lucianobrsts <lucianobrsts@yahoo.com.br>
2023-04-15 21:55:42 -0300

    Criados pacotes para requestBody

---
+++ src/main/java/academy/devdojo/springboot2/request/AnimePostRequsetBody.java
@@ -0,0 +1,8 @@
+package academy.devdojo.springboot2.request;
+
+import lombok.Data;
+
+@Data
+public class AnimePostRequsetBody {
+    private String name;
+}
---
+++ src/main/java/academy/devdojo/springboot2/request/AnimePutRequsetBody.java
@@ -0,0 +1,9 @@
+package academy.devdojo.springboot2.request;
+
+import lombok.Data;
+
+@Data
+public class AnimePutRequsetBody {
+    private Long id;
+    private String name;
+}
